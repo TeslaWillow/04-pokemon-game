5dd5523e5be5e0ade99144660bfccf268c4eb5d3
TeslaWillow <hectorjosue.caballero@gmail.com>
2024-10-09 21:37:43 -0600

    show correct options and wrong options.

--- src/modules/pokemon/components/PokemonOptions.vue
+++ src/modules/pokemon/components/PokemonOptions.vue
@@ -3,8 +3,12 @@
         <button 
             v-for="{ id, name } in options"
             :key="id"
+            :disabled="blockSelection"
+            :class="['option', {
+                correct:   blockSelection && id === correctAnswer.id,
+                incorrect: blockSelection && id !== correctAnswer.id,
+            }]"
             @click="$emit('selectedOption', id)"
-            class="capitalize"
         >
             {{ name }}
         </button>
@@ -17,6 +21,8 @@ import type { Pokemon } from '../interfaces';
 // Inputs
 interface Props {
     options: Pokemon[];
+    correctAnswer: Pokemon;
+    blockSelection: boolean;
 }
 defineProps<Props>();
 
@@ -30,5 +36,14 @@ defineEmits<{
 <style lang="css" scoped>
 button {
     @apply bg-white shadow-md rounded-lg p-3 mb-5 cursor-pointer w-80 text-center transition-all hover:bg-gray-100;
+}
+.option {
+    @apply capitalize disabled:shadow-none disabled:cursor-not-allowed disabled:pointer-events-none;
+}
+.correct {
+    @apply bg-green-700 text-white;
+}
+.incorrect {
+    @apply bg-red-100 opacity-70;
 }
 </style>
--- src/modules/pokemon/pages/PokemonGame.vue
+++ src/modules/pokemon/pages/PokemonGame.vue
@@ -6,7 +6,7 @@
 
     <section v-else class="flex flex-col justify-center items-center w-screen h-screen">
         <div class="p-4 text-sm text-gray-800 rounded-lg bg-gray-50 dark:bg-gray-800 dark:text-gray-300" role="alert">
-            <span class="font-medium">{{ gameStatus }}</span>
+            <span class="font-medium capitalize">{{ gameStatus }}</span>
         </div>
 
         <h1 class="text-3xl mb-5">¿Quién es este pokemon?</h1>
@@ -20,6 +20,8 @@
         <!-- POKEMON OPTIONS  -->
         <PokemonOptions 
             :options="options"
+            :correct-answer="randomPokemon"
+            :block-selection="gameStatus !== GameStatus.Playing"
             @selected-option="onSelectedOptions"
         />
         
